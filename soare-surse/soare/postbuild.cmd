8d4b89c799e24d36bc25de6656ce794f34575640
Cristi Anichitei <cristi.anichitei@gmail.com>
2016-10-17 10:55:22 +0300

    Update postbuild.cmd to copy soare.bin to the grub folder
    Note: bash -c "grub-mkrescue" appears to not be working when called at postbuild, so this is not a fully automated process just yet

--- soare-surse/soare/postbuild.cmd
+++ soare-surse/soare/postbuild.cmd
@@ -3,16 +3,8 @@
 :: must be called as postbuild.cmd <solutiondir> <targetbin>
 ::
 
-:: rem :: Needed only if you use the custom VM -> delete all "rem"
-:: 
-:: rem set target_mac=00.50.56.2C.D5.76
-:: 
-:: rem echo POSTBUILD: copying %2 to c:\VM\tftpfolder\%target_mac%.bin
-:: rem copy %2 c:\VM\tftpfolder\%target_mac%.bin
-:: 
-:: echo POSTBUILD: will insert %2 into SOARE2-FLAT.VMDK...
-:: echo IMPORTANT: if the Disk is running under VMWARE, you MUST stop it to succeed!
-:: 
-:: vmware-mount.exe z: D:\SISC\Sem1\PSNA\miniOS\soare-tools\soare2.vmdk
-:: copy %2 z:\soare.bin
-:: vmware-mount.exe z: /d
+cd ..\..\soare-tools\
+rm soare-iso\boot\soare.bin
+cp %2 soare-iso\boot\soare.bin
+:: ls soare-iso\boot
+:: bash -c "./create-iso.sh"
