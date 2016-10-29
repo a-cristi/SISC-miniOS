1dd1f2a1f08171ed8ee5aa9411368dbb04b2a433
Cristi Anichitei <cristi.anichitei@gmail.com>
2016-10-29 13:10:40 +0300

    Switch to long mode and call the C erntry point
    Use temporary identity mappings for now (the final tables will probably be created from C once we have a memory map)

--- soare-surse/soare/main.c
+++ soare-surse/soare/main.c
@@ -1,8 +1,11 @@
 #include "boot.h"
 #include "screen.h"
 
+extern void __DbgBochsBreak(void);
+
 void EntryPoint(void)
 {
     VgaInit(VGA_MEMORY_BUFFER, vgaColorWhite, vgaColorBlack);
+    VgaPutString("Hello");
     __halt();
 }
